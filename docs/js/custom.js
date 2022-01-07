9c19dc545381334514e03077ba1e145873e0546c
STARRY-S <hxstarrys@gmail.com>
2022-01-07 13:10:14 +0800

    Added Google Analytics.

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,17 @@
 "use strict";
 // Write your custom JavaScript code here.
+
+function addScript(url){
+	var script = document.createElement("script");
+	script.setAttribute("type","text/javascript");
+	script.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-70DHXLE02Z");
+    script.setAttribute("async");
+	document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+addScript();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag("js", new Date());
+
+gtag("config", "G-70DHXLE02Z");
