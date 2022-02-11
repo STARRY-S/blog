4a5890391abcbd4296582bac49149d9bb308b79e
STARRY-S <hxstarrys@gmail.com>
2022-02-11 20:35:45 +0800

    Update Site.

---
+++ docs/js/custom.js
@@ -0,0 +1,18 @@
+"use strict";
+// Write your custom JavaScript code here.
+
+
+function addScript(url){
+	var script = document.createElement("script");
+	script.setAttribute("type","text/javascript");
+	script.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-70DHXLE02Z");
+    // script.setAttribute("async");
+    document.head.appendChild(script);
+}
+
+addScript();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag("js", new Date());
+
+gtag("config", "G-70DHXLE02Z");
