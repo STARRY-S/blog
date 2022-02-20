1ea99a4f32c7d633b91ce44dd69675e110f1e15f
STARRY-S <hxstarrys@gmail.com>
2022-02-20 12:48:05 +0800

    Remove period from the title of commit message

--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-hikaru clean
-hikaru build --debug
-git add ./docs/
-git commit -m "Updated Site."
-git push origin main
-# git push gitlab main
--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ hikaru clean
 hikaru build --debug
 
 git add ./docs/
-git commit -m "Updated generated site files."
+git commit -m "Updated generated site files"
 
 git push origin main
 # git push gitlab main
