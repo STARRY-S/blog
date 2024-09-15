e0ed2e8f988e54906c550ecad1bc9655f90233fc
STARRY-S <hxstarrys@gmail.com>
2024-09-15 16:10:28 +0800

    Update site

--- static/js/home-info.js
+++ static/js/home-info.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function randomHomeInfo() {
-    let zh = [
+    let infos = [
         "情况嘛就是这么个情况，具体什么情况还得看情况",
         "这么晚还没睡的人应该还没睡吧",
         "在令人失望这件事情上，咱从来不让人失望",
@@ -9,20 +9,8 @@ function randomHomeInfo() {
         "努力不一定被别人看到，但休息一定会",
         "后天的努力最重要，今天和明天先睡大觉",
         "<code>cat /dev/random > /dev/null</code>",
-        "小心福瑞控",
-        "这里是福瑞控，该小心的是你",
+        "谁都可以睡，但是不能笑",
     ];
-    let en = [
-        "Morning.",
-        "Don't try to be a hero.",
-        "<code>cat /dev/random > /dev/null</code>",
-    ]
-
-    let infos = zh;
-    const pathname = window.location.pathname;
-    if (pathname.startsWith("/en/")) {
-        infos = en;
-    }
     return infos[Math.floor(Math.random() * infos.length)];
 }
 
